04a70cd9eac117a5a5b4820e8d0378a0b9bec02a
Ethereal Element <vitalik.safonov@list.ru>
2022-09-22 14:54:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log(4 + "5");
+
+let inr = 5,
+  dec = 5;
+
+// inr++;
+// dec--;
+console.log(++dec);
+console.log(--inr);
+console.log(5 % 2);
+console.log(2 * 4);
+console.log(8 * 8 === 63);
+
+const isChecked = true,
+  isClose = false;
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 === 6);
