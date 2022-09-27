e19910c421555c7c7035bd03171be19f6d5233a2
Ethereal Element <vitalik.safonov@list.ru>
2022-09-27 22:06:12 +0300

    создание циклов и условий

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 "use strict";
 
-console.log(4 + "5");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-let inr = 5,
-  dec = 5;
+const personalMovieDB = {
+  count: {},
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// inr++;
-// dec--;
-console.log(++dec);
-console.log(--inr);
-console.log(5 % 2);
-console.log(2 * 4);
-console.log(8 * 8 === 63);
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?");
 
-const isChecked = true,
-  isClose = false;
-console.log(isChecked || !isClose);
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+  personalMovieDB.movies[a] = b;
+}
 
-console.log(2 + 2 * 2 === 6);
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено слишком фильмов!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман!");
+} else {
+  console.log("Произошла ошибка!");
+}
+
+console.log(personalMovieDB);
