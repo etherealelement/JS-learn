bd8379d5b041ba6f0729b34653e7a79005ba8649
Ethereal Element <vitalik.safonov@list.ru>
2022-10-01 15:51:57 +0300

    add file objects.js

---
+++ examlpes-js/call-back.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function first() {
+  // какие то действия
+  setTimeout(() => {
+    console.log(1);
+  }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+//
+
+function learnJS(lang, callback) {
+  console.log(`Я учу: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log("Я прошел этот урок");
+}
+
+learnJS("JS", done);
