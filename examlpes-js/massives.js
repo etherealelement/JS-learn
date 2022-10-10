3dd7a5c7ecad657ac20410ed3d530f9aa832e6b0
Ethereal Element <vitalik.safonov@list.ru>
2022-10-10 21:38:25 +0300

    massives

--- examlpes-js/massives.js
+++ examlpes-js/massives.js
@@ -21,12 +21,34 @@ function standardizeStrings(arr) {
 standardizeStrings(favoriteCities);
 
 //
+//
 const someString = "This is some strange string";
 
 function reverse(str) {
   if (typeof str !== "string") {
     return "Ошибка!";
   }
-
-  return str.split("").reverse().join("");
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
 }
+reverse(someString);
+//
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+
+function availableCurr(arr, missingCurr) {
+  let str = "";
+  arr.lenght === 0
+    ? (str = "Нет доступных валют")
+    : (str = "Доступные валюты: \n");
+  arr.forEach((curr, i) => {
+    if (curr !== missingCurr) {
+      str += `${curr} \n`;
+    }
+  });
+  return str;
+}
+availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY");
